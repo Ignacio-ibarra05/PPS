2a62adcffaa50cb70c0fee142b9b2f2fcef8e381
Pablo Ahumada <pablo.ahumada1@mail.udp.cl>
2024-12-15 04:39:24 -0300

    coef r, tester added

--- coef.py
+++ coef.py
@@ -1,5 +1,6 @@
 import requests
 from functools import reduce
+from tester import *
 
 API_BASE_URL = "https://api.guildwars2.com/v2"
 
@@ -17,14 +18,6 @@ def get_data_API(endpoint):
         return response.json()
     return []
 
-Power = 0
-Power2 = 0
-Burning = 0
-Bleeding = 0
-Poison = 0
-Torment = 0
-Confusion = 0
-
 
 condition_ids = {
     "Burning": 737,
@@ -34,101 +27,160 @@ condition_ids = {
     "Confusion": 861,
 }
 
+def getCoef(url,input_stat):
+    coef_result = {
+        'Power': 0,
+        'Burning': 0,
+        'Bleeding': 0,
+        'Poison': 0,
+        'Torment': 0,
+        'Confusion': 0,
+        'Crit': 0,
+        'Tarmor': 0,
+        'duracion': 0
+    }
 
-url = 'https://dps.report/getJson?permalink=https://dps.report/LzVj-20231003-151324_golem'
-data = get_data(url)
+    urldata = 'https://dps.report/getJson?permalink=' + url
+    data = get_data(urldata)
 
-totalDps = 0
+    totalDps = 0
 
-end = data['phases'][0]['end']
-start = data['phases'][0]['start']
-duration = (end -start)/1000
+    end = data['phases'][0]['end']
+    start = data['phases'][0]['start']
+    duration = (end -start)/1000
+    coef_result['duracion'] = duration
+    powerdps = data['players'][0]['dpsTargets'][0][0]['powerDps']
+    powerdamage = data['players'][0]['dpsTargets'][0][0]['powerDamage']
 
-powerdps = data['players'][0]['dpsTargets'][0][0]['powerDps']
+    tArmor = data['targets'][0]['toughness']
+    coef_result['Tarmor'] = tArmor
+    critRate = data['players'][0]['statsAll'][0]['criticalRate']/data['players'][0]['statsAll'][0]['critableDirectDamageCount']
 
-print(f'duracion: {duration}')
-print(f'powerDPS: {powerdps}')
-damage_dist = data['players'][0]['targetDamageDist'][0][0]
+    coef_result['Crit'] = critRate - input_stat['crit_chance']
 
-condition_data = {}
+    damage_dist = data['players'][0]['targetDamageDist'][0][0]
 
-# Procesamiento de las condiciones
-for condition, condition_id in condition_ids.items():
-    entry = next((e for e in damage_dist if e.get("id") == condition_id), None)
-    total_damage = entry.get("totalDamage", 0) if entry else 0
-    dps = round((total_damage / duration), 2)
-    condition_data[condition] = dps
+    condition_data = {}
 
-print(condition_data)
+    # Procesamiento de las condiciones
+    for condition, condition_id in condition_ids.items():
+        entry = next((e for e in damage_dist if e.get("id") == condition_id), None)
+        total_damage = entry.get("totalDamage", 0) if entry else 0
+        dps = round((total_damage / duration), 2)
+        condition_data[condition] = dps
+        match condition:
+            case 'Burning': # 0.155 * Condition Damage + 131
+                dps1 = input_stat['condi_dmg'] * 0.155 + 131
+                dps2 = dps1 * input_stat['expertice_quemado']
 
-# Obtiene las entradas de buffs no relacionados con condiciones
-non_condition_buff_entries = [
-    (int(buff_id.replace('b', '')), buff_data['name'])
-    for buff_id, buff_data in data['buffMap'].items()
-    if int(buff_id.replace('b', '')) not in condition_ids.values()
-]
+            case 'Bleeding': # 0.06 * Condition Damage + 22
+                dps1 = input_stat['condi_dmg'] * 0.06 + 22
+                dps2 = dps1 * input_stat['expertice_sangrado']
 
-# Procesa las entradas no relacionadas con condiciones
-non_condition_data_entries = []
-for buff_id, name in non_condition_buff_entries:
+            case 'Poison': # 0.06 * Condition Damage + 33.5
+                dps1 = input_stat['condi_dmg'] * 0.06 + 33.5
+                dps2 = dps1 * input_stat['expertice_veneno']
 
-    entry = next((e for e in damage_dist if e.get("id") == buff_id), {})
-    total_damage = entry.get("totalDamage", 0)
-    connected_hits = entry.get("connectedHits", 0)
-    
-    if not total_damage:
-        continue
+            case 'Torment': # 0.09 * Condition Damage + 31.8
+                dps1 = input_stat['condi_dmg'] * 0.09 + 31.8
+                dps2 = dps1 * input_stat['expertice_tormento']
 
-    dps = round((total_damage / duration), 2)
-    hits_per_second = round((connected_hits / duration), 2)
+            case 'Confusion': # 0.05 * Condition Damage + 18.25
+                dps1 = input_stat['condi_dmg'] * 0.05 + 18.25
+                dps2 = dps1 * input_stat['expertice_confusion']
+        coef_result[condition] = condition_data[condition]/dps2
 
-    non_condition_data_entries.append((f'{name} "Power" DPS ({hits_per_second} hit/sec)', dps))
+    # Obtiene las entradas de buffs no relacionados con condiciones
+    non_condition_buff_entries = [
+        (int(buff_id.replace('b', '')), buff_data['name'])
+        for buff_id, buff_data in data['buffMap'].items()
+        if int(buff_id.replace('b', '')) not in condition_ids.values()
+    ]
 
+    # Procesa las entradas no relacionadas con condiciones
+    non_condition_data_entries = []
+    for buff_id, name in non_condition_buff_entries:
 
-possibleLifestealDamageSum = reduce(lambda prev, item: prev + item[1], non_condition_data_entries, 0)
+        entry = next((e for e in damage_dist if e.get("id") == buff_id), {})
+        total_damage = entry.get("totalDamage", 0)
+        connected_hits = entry.get("connectedHits", 0)
+        
+        if not total_damage:
+            continue
 
-powerDPSWithoutLifesteal = powerdps - possibleLifestealDamageSum
+        dps = round((total_damage / duration), 2)
+        hits_per_second = round((connected_hits / duration), 2)
 
-print(f'Power: {powerDPSWithoutLifesteal}')
-
-print(condition_data)
-
-for i in condition_data:
-    totalDps += condition_data[i]
-
-totalDps += powerDPSWithoutLifesteal
-
-
-
-print(totalDps)
-print('\n')
-print(str(data.keys()))
-print('\n')
+        non_condition_data_entries.append((f'{name} "Power" DPS ({hits_per_second} hit/sec)', dps))
 
 
-print(data['players'][0]['rotation'][0])
-print('\n')
+    possibleLifestealDamageSum = reduce(lambda prev, item: prev + item[1], non_condition_data_entries, 0)
 
-coef_dmg = 0
+    powerDPSWithoutLifesteal = powerdps - possibleLifestealDamageSum
 
-for skill in data['players'][0]['rotation']:
-    id = skill['id']
-    print(f'Skill id: { id }')
-    skill_hits = len(skill['skills'])
-    print(f'Skill hits: { skill_hits }')
+    for i in condition_data:
+        totalDps += condition_data[i]
 
-    skillAPIData = get_data_API(f'skills/{id}')
-    if isinstance(skillAPIData, dict) and 'facts' in skillAPIData:
-        damage_facts = [fact for fact in skillAPIData['facts'] if fact.get('type') == 'Damage']
-        dmg_m = 0
-        if damage_facts:
-            for d in damage_facts:
-                if d['dmg_multiplier'] > dmg_m:
-                    dmg_m = d['dmg_multiplier']*d['hit_count']
-        print(f'Damage modifier { dmg_m }')
-    coef_dmg += skill_hits*dmg_m
-    print('\n')
+    totalDps += powerDPSWithoutLifesteal
 
-coef_dmg = coef_dmg*1000/duration
-print(coef_dmg)
 
+    coef_result['Power'] = powerdps / ((1 - critRate) * (1000 * input_stat['power'] / tArmor) + critRate * input_stat['crit_dmg'] * (1000 * input_stat['power'] / tArmor))
+    return coef_result
+
+
+
+input_stat = {
+    'power': 3103,
+    'precision': 1813,
+    'crit_chance': 0.6871,
+    'crit_dmg': 1.5,
+    'condi_dmg': 2924,
+    'expertice': 747,
+    'expertice_tormento': 1.798,
+    'expertice_sangrado': 1.848,
+    'expertice_quemado': 1.648,
+    'expertice_veneno': 1.648,
+    'expertice_confusion': 1.648
+}
+coef = getCoef('https://dps.report/LzVj-20231003-151324_golem',input_stat)
+
+input_stat = {
+    'power': 2820,
+    'precision': 2155,
+    'crit_chance': 1,
+    'crit_dmg': 1.7167,
+    'condi_dmg': 2392,
+    'expertice': 747,
+    'expertice_tormento': 1.35,
+    'expertice_sangrado': 1.85,
+    'expertice_quemado': 1.35,
+    'expertice_veneno': 1.35,
+    'expertice_confusion': 1.35
+}
+coef = getCoef('https://dps.report/YeGx-20231205-223957_golem',input_stat)
+
+input_stat_2 = {
+    'power': 2820,
+    'precision': 2155,
+    'crit_chance': 1,
+    'crit_dmg': 1.7167,
+    'condi_dmg': 2402,
+    'expertice': 747,
+    'expertice_tormento': 1.35,
+    'expertice_sangrado': 1.85,
+    'expertice_quemado': 1.35,
+    'expertice_veneno': 1.35,
+    'expertice_confusion': 1.35
+}
+
+print("\n")
+print(f'Entrada de estadisticas 1: { input_stat }')
+print("\n")
+print(f'Coeficientes obtenidos: { coef }')
+print("\n")
+print(f'Daño para la entrada 1: {calcular_dano(coef, input_stat)}')
+print("\n")
+print(f'Entrada de estadisticas 2: { input_stat_2 }')
+print("\n")
+print(f'Daño para la entrada 2: {calcular_dano(coef, input_stat_2)}')
+print("\n")
---
+++ tester.py
@@ -0,0 +1,43 @@
+def calcular_dano_fisico(coef_habilidad, power_stat, prob_crit, dano_crit, armadura_objetivo, duracion):
+    crit = prob_crit
+    if prob_crit > 1:
+        crit = 1
+    dano_normal = 1000 * coef_habilidad * power_stat / armadura_objetivo
+    dano_critico = dano_normal * dano_crit
+    dano = dano_normal * (1 - crit) + dano_critico * crit
+    return dano * duracion
+
+def calcular_dano_condi(coef_burning, coef_bleeding, coef_poison, coef_torment, coef_confusion, condi_stat, exp_burning, exp_bleeding, exp_poison, exp_torment, exp_confusion, duracion):
+    dano = 0
+    dano += (condi_stat * 0.155 + 131) * exp_burning * coef_burning
+    dano += (condi_stat * 0.06 + 22) * exp_bleeding * coef_bleeding
+    dano += (condi_stat * 0.06 + 33.5) * exp_poison * coef_poison
+    dano += (condi_stat * 0.09 + 31.8) * exp_torment * coef_torment
+    dano += (condi_stat * 0.05 + 18.25) * exp_confusion * coef_confusion
+    return dano * duracion
+
+def calcular_dano(coef, stat):
+    coef_habilidad = coef['Power']
+    power_stat = stat['power']
+    prob_crit = coef['Crit'] + stat['crit_chance']
+    dano_crit = stat['crit_dmg']
+    armadura_objetivo = coef['Tarmor']
+    duracion = coef['duracion']
+    
+    dano_fisico = calcular_dano_fisico(coef_habilidad, power_stat, prob_crit, dano_crit, armadura_objetivo, duracion)
+
+    coef_burning = coef['Burning']
+    coef_bleeding = coef['Bleeding']
+    coef_poison = coef['Poison']
+    coef_torment = coef['Torment']
+    coef_confusion = coef['Confusion']
+    condi_stat = stat['condi_dmg']
+    exp_burning = stat['expertice_quemado']
+    exp_bleeding = stat['expertice_sangrado']
+    exp_poison = stat['expertice_veneno']
+    exp_torment = stat['expertice_tormento']
+    exp_confusion = stat['expertice_confusion']
+
+    dano_condi = calcular_dano_condi(coef_burning, coef_bleeding, coef_poison, coef_torment, coef_confusion, condi_stat, exp_burning, exp_bleeding, exp_poison, exp_torment, exp_confusion, duracion)
+
+    return dano_condi + dano_fisico
